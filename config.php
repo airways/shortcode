f5193819866e5167891b06df15bf9665a704cb64
Isaac Raway <isaac@mm.st>
2012-09-23 04:05:21 -0500

    Bump version Update prolib dependency

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-// @version 1.02
-// @prolib 0.47
+// @version 1.03
+// @prolib 0.54
 
-define('SHORTCODE_VERSION',       '1.02');
+define('SHORTCODE_VERSION',       '1.03');
 define('SHORTCODE_NAME',          'Shortcode');
 define('SHORTCODE_CLASS',         'Shortcode'); // must match module class name
 define('SHORTCODE_DESCRIPTION',   'Provides shortcode macros which can be set by members on a global, site, and member-specific basis as well provided by plugins and modules.');
